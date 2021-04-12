36b892ff10e6c0b0a4e603cc2d1603dd598eaf37
Tuhgtuhgtuhg <tuhgtuhgtuhg@gmail.com>
2021-04-12 22:44:59 +0300

    Added implementation of all functions for laboratory work No. 9.

--- lab08/prj/ModulesPROKOPENKO/modulesprokopenko.cpp
+++ lab08/prj/ModulesPROKOPENKO/modulesprokopenko.cpp
@@ -6,6 +6,66 @@
 #include <windows.h>
 #include <wchar.h>
 #include <locale.h>
+#include <vector>
+struct forTask9_1{
+    std::wstring character = L"", speed = L"";
+};
+
+struct forTask9_2{
+    long nPowM = 0;
+    long mPowN = 0;
+    std::vector<int> forNToM;
+};
+
+forTask9_1 task9_1( char ball ){
+    struct forTask9_1 result;
+    switch (ball) {
+    case 0: result.speed = L"< 0.3";
+        result.character = L"Штиль";
+        break;
+    case 1: result.speed = L"0.3 - 1.5";
+        result.character = L"Тихий";
+        break;
+    case 2: result.speed = L"1.6 - 3.4";
+        result.character = L"Легкий";
+        break;
+    case 3: result.speed = L"3,5 - 5,4";
+        result.character = L"Слабкий";
+        break;
+    case 4: result.speed = L"5,5 - 7,9";
+        result.character = L"Помірний";
+        break;
+    case 5: result.speed = L"8,0 - 10,7";
+        result.character = L"Свіжий";
+        break;
+    case 6: result.speed = L"10,8 - 13,8";
+        result.character = L"Сильний";
+        break;
+    case 7: result.speed = L"13,9 - 17,1";
+        result.character = L"Міцний";
+        break;
+    case 8: result.speed = L"17,2 - 20,7";
+        result.character = L"Дуже міцний";
+        break;
+    case 9: result.speed = L"20,8 - 24,4";
+        result.character = L"Шторм";
+        break;
+    case 10: result.speed = L"24,5 - 28,4";
+        result.character = L"Сильний шторм";
+        break;
+    case 11: result.speed = L"28,5 - 32,6";
+        result.character = L"Жорстокий шторм";
+        break;
+    case 12: result.speed = L"⩾ 32,6";
+        result.character = L"Ураган (Буревій)";
+        break;
+    default: result.speed = L"None";
+        result.character = L"None";
+        break;
+    }
+    return result;
+}
+
 
 
 double s_calculation(float x, float y, float z)
@@ -31,4 +91,41 @@ void consoleToUtf8()
     _wsetlocale(LC_ALL,L"");
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
+}
+
+forTask9_2 task9_2(int n, int m)
+{
+    forTask9_2 result;
+    result.mPowN = pow(m,n);
+    result.nPowM = pow(n,m);
+    for ( int i = n; i <= m; i++)
+        if (i % 2 == i)
+            result.forNToM.push_back(i);
+    return result;
+}
+
+int task9_3(int N){
+    int result = 0;
+    bool flag = false;
+    for (int n=sizeof(int)*8-1; n>-1; n--)  {
+        if((N>>n)&1)
+            if(!flag) {
+                flag = true;
+            }
+        if (flag){
+            if((N>>1)&1){
+                result += ((N>>n)&1)? 1 : 0;
+            }else{
+                result += ((N>>n)&1)? 0 : 1;
+            }
+        }
+    }
+    return result;
+}
+
+std::wstring devInfo(){
+    std::wstring info = L"╔══════════════════════════════════════════════════════╗\n"
+                         "║Виконав студент групи КБ20 Прокопенко Єгор Сергійович©║\n"
+                         "╚══════════════════════════════════════════════════════╝\n";
+    return info;
 }
--- lab08/prj/ModulesPROKOPENKO/modulesprokopenko.h
+++ lab08/prj/ModulesPROKOPENKO/modulesprokopenko.h
@@ -1,7 +1,12 @@
 #ifndef MODULESPROKOPENKO_H
 #define MODULESPROKOPENKO_H
-
+#include<iostream>
+struct forTask9_1;
+struct forTask9_2;
 void consoleToUtf8();
 double s_calculation(float x, float y, float z);
-
+int task9_3(int N);
+struct forTask9_1 task9_1(char ball);
+struct forTask9_2 task9_2(int n, int m);
+std::wstring devInfo();
 #endif // MODULESPROKOPENKO_H
